df3fda2c9d123f3640bd3ce7d46e980bc6ac1500
Ikram Hasan <ikramhasan.dev@gmail.com>
2024-03-06 15:25:04 +0600

    update: use deepseek coder for coding scripts

--- ai_commit.sh
+++
@@ -1,5 +0,0 @@
-# AI Commit Message
-ai_commit() {
- diff=$(git diff --cached)
- ollama run mistral:7b-instruct "Diff: $diff. Suggest 3 commit messages for the diff provided above in a list format. commit messages should follow conventional commit style, and the format should be <type>[scope]: <description>. Example: fix(authentication): add password regex pattern."
-}
--- ai_pr.sh
+++
@@ -1,5 +0,0 @@
-# AI PR Review
-ai_pr () {
-  diff=$(gh pr diff "$1")
-  ollama run mistral:7b-instruct "Provide a brief summary of this PR, and highlight any violations of security and coding best practices in the following git changes diff: $diff"
-}
